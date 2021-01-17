32b257165fa91b5bc16c6c12426b5150e3434203
Smile Gupta <guptamiley3012@gmail.com>
2021-01-18 03:10:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
+const axios = require("axios");
+const authorizationToken = process.env.authorizationToken;
+
 exports.handler = (event, context, callback) => {
-return event
-}
+    let headerMessage, styleType;
+    if (event.body.status === "success") {
+        headerMessage = `Deployment to ${event.body.environment} environment is successful`;
+        styleType = "SUCCESS";
+    } else {
+        headerMessage = `Deployment to ${event.body.environment} environment was not successful`;
+        styleType = "ERROR";
+    }
+
+    if (event.body.status === "running")
+        return {
+            statusCode: 200,
+            body: "Pipeline is still running"
+        };
+
+    return axios
+        .post(
+            "https://konfhub.jetbrains.space/api/http/chats/messages/send-message",
+            {
+                recipient: {
+                    className: "MessageRecipient.Member",
+                    member: "username:Srushith"
+                },
+                content: {
+                    className: "ChatMessage.Block",
+                    style: styleType,
+                    outline: {
+                        icon: { icon: "merge-request-small" },
+                        text: "Deployment Status"
+                    },
+                    sections: [
+                        {
+                            className: "MessageSection",
+                            header: headerMessage,
+                            elements: [],
+                            footer: `Job Details: ${event.body.deployable_url}`
+                        }
+                    ]
+                }
+            },
+            {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: authorizationToken
+                }
+            }
+        )
+        .then((response) => {
+            return {
+                statusCode: 200,
+                body: "Message triggered successfully"
+            };
+        })
+        .catch((error) => {
+            console.log("Error", error);
+        });
+};
